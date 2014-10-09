a79ce329005b705f588d9540c3d50ecb73011514
jorgevs <jevzsz@gmail.com>
2014-10-08 21:39:37 -0400

    main method in Duck.java

--- src/main/java/com/jvs/git/Duck.java
+++ src/main/java/com/jvs/git/Duck.java
@@ -2,6 +2,11 @@ package com.jvs.git;
 
 public class Duck extends Animal {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
 	@Override
 	public String makeNoise() {
 		// TODO Auto-generated method stub
